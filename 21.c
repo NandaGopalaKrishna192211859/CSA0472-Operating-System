6a7ba26840cbd5f5388546f7d1cb0561ed0c62c7
NandaGopalaKrishna <123743796+NandaGopalaKrishna192211859@users.noreply.github.com>
2023-11-21 21:42:19 +0530

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#define MEMORY_SIZE 1000
+
+void worstFit(int blockSize[], int m, int processSize[], int n) {
+    int allocation[n];
+
+    for (int i = 0; i < n; i++) {
+        allocation[i] = -1;  // Initialize all processes to -1 (not allocated)
+    }
+
+    for (int i = 0; i < n; i++) {
+        int worstIdx = -1;
+
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                if (worstIdx == -1 || blockSize[j] > blockSize[worstIdx]) {
+                    worstIdx = j;
+                }
+            }
+        }
+
+        if (worstIdx != -1) {
+            allocation[i] = worstIdx;
+            blockSize[worstIdx] -= processSize[i];
+        }
+    }
+
+    printf("Process\tProcess Size\tBlock Index\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t", i + 1, processSize[i]);
+        if (allocation[i] != -1) {
+            printf("%d\n", allocation[i]);
+        } else {
+            printf("Not Allocated\n");
+        }
+    }
+}
+
+int main() {
+    // Example data
+    int blockSize[] = {100, 500, 200, 300, 600};
+    int processSize[] = {212, 417, 112, 426};
+
+    int m = sizeof(blockSize) / sizeof(blockSize[0]);
+    int n = sizeof(processSize) / sizeof(processSize[0]);
+
+    worstFit(blockSize, m, processSize, n);
+
+    return 0;
+}
